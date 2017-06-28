202d04ebce2ef0531c0f5405620a9c2fc9dfd2f0
Tim Shenin <tshenin@solit-clouds.ru>
2017-06-28 19:04:53 +0300

    Add method to find neighbours to cell

--- src/main/java/builder/Labyrinth.java
+++ src/main/java/builder/Labyrinth.java
@@ -3,6 +3,7 @@ package builder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Labyrinth {
 
@@ -25,7 +26,7 @@ public class Labyrinth {
         for(int x = 0; x < dimensionX; x++)
             for(int y = 0; y < dimensionY; y++) {
                 boolean isEvenCell = (x + y) % 2 == 0;
-                matrix.add(new Cell(x, y, isEvenCell, false));
+                matrix.add(new Cell(x, y, !isEvenCell, false));
             }
 
         return matrix;
@@ -38,21 +39,29 @@ public class Labyrinth {
     }
 
     private void findNotVisitedAndStandOnIt(Cell currentCell, List<Cell> matrix) {
+        List<Cell> unvisitedNeighbours = getUnvisitedNeighbours(currentCell, matrix);
+        if (unvisitedNeighbours.size() > 0) {
+            Random randomGenerator = new Random();
+            int randIndex = randomGenerator.nextInt(unvisitedNeighbours.size());
+            matrix = removeWall(currentCell, unvisitedNeighbours.get(randIndex), matrix);
 
+            currentCell = unvisitedNeighbours.get(randIndex);
+            currentCell.setVisited(true);
+        }
     }
 
-    private List<Cell> getNeighbours(Cell cell, List<Cell> matrix) {
+    private List<Cell> getUnvisitedNeighbours(Cell cell, List<Cell> matrix) {
         List<Cell> neighbours = new ArrayList<Cell>();
         int x = cell.getX();
         int y = cell.getY();
-        int[] up = {x, y - 1};
-        int[] rt = {x + 1, y};
-        int[] dn = {x, y + 1};
-        int[] lt = {x - 1, y};
+        int[] up = {x, y - 2};
+        int[] rt = {x + 2, y};
+        int[] dn = {x, y + 2};
+        int[] lt = {x - 2, y};
         int[][] d = {up, rt, dn, lt};
 
         for(int i = 0; i < 4; i++) {
-            if(d[i][0] > 0 && d[i][1] < this.dimensionX && d[i][1] > 0 && d[i][1] < this.dimensionY) {
+            if(d[i][0] >= 0 && d[i][1] < this.dimensionY && d[i][1] >= 0 && d[i][0] < this.dimensionX) {
 
                 Cell neighbour = getMatrixArrayIndexByCoords(d[i][0], d[i][1], matrix);
 
@@ -63,6 +72,21 @@ public class Labyrinth {
         }
 
         return neighbours;
+    }
+
+    private List<Cell> removeWall(Cell cellA, Cell cellB, List<Cell> matrix) {
+        int diffX = cellA.getX() - cellB.getX();
+        int diffY = cellA.getX() - cellB.getX();
+        int addX, addY;
+
+        addX = diffX != 0 ? diffX / Math.abs(diffX) : 0;
+        addY = diffY != 0 ? diffY / Math.abs(diffY) : 0;
+
+        Cell targetCell = getMatrixArrayIndexByCoords(cellA.getX() + addX, cellA.getY() + addY, matrix);
+        targetCell.setWall(false);
+        targetCell.setVisited(true);
+
+        return matrix;
     }
 
     private Cell getMatrixArrayIndexByCoords(int x, int y, List<Cell> matrix) {
